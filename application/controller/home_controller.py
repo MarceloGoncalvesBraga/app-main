8311b0ca7160a8b454460fc9f90a23f26ada9f71
MarceloGoncalvesBraga <85029465+MarceloGoncalvesBraga@users.noreply.github.com>
2022-05-03 19:37:39 -0300

    Update home_controller.py

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -8,3 +8,10 @@ def home():
 @app.route("/test", methods=['GET'])
 def test():
 nome = request.arg['meuparametro']
+
+@app.route("/usuario", methods=['GET', 'POST'])
+def inserir():
+dados_post = request.data
+
+@app.route("/usuario", methods=['GET', 'POST'])
+def inserir():
